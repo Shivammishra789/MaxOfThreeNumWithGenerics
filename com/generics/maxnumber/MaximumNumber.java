064cfe3c80f26bf92d5ae08115e0ee7179173c5a
Shivammishra789 <90849543+Shivammishra789@users.noreply.github.com>
2021-10-28 15:38:50 +0530

    generic method to find maximum for all 3 problems with one method

--- com/generics/maxnumber/MaximumNumber.java
+++ com/generics/maxnumber/MaximumNumber.java
@@ -13,7 +13,8 @@ public class MaximumNumber {
 		E max = num1; // assigning first number to max
 		if (num2.compareTo(max) > 0) { // comparing second number with max and if greater assigning it to max
 			max = num2;
-		} else if (num3.compareTo(max) > 0) {
+		}
+		if (num3.compareTo(max) > 0) {
 			max = num3;
 		}
 		System.out.println("Maximum is " + max); // printing max of all three
