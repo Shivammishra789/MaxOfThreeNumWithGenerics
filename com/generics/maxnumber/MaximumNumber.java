8a3eec2bf481a541d37016f52332b48ff895b9ec
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-29 11:51:32 +0530

    printing max value with generic method

--- com/generics/maxnumber/MaximumNumber.java
+++ com/generics/maxnumber/MaximumNumber.java
@@ -5,21 +5,25 @@ public class MaximumNumber{
 	public static void main(String[] args) {
 
 		Integer[] intArray = {5,7,8,12,89};
-		findMaximum(intArray); // passing integer array to method
 		Float[] floatArray = {5.7f,7.4f,8.9f,12.7f,8.9f};
-		findMaximum(floatArray);
 		String[] stringArray = {"Shivam","orange","Goa","carrot","apple"};
+		
+		findMaximum(intArray); 
+		findMaximum(floatArray);
 		findMaximum(stringArray);
 	}
 
-	public static <E extends Comparable<E>> void findMaximum(E[] array) { // passing generic array to method
+	public static <E extends Comparable<E>> void findMaximum(E[] array) { 
 
 		E max = array[0]; // assigning first array value to max
 		for(int i = 1; i < array.length; i++) {
-			if (max.compareTo(array[i]) < 0) { // comparing second number with max and if greater assigning it to max
+			if (max.compareTo(array[i]) < 0) { 
 				max = array[i];
 			}
 		}
+		printMax(max);  // calling generic printMax method
+	}
+	public static <E> void printMax(E max) {
 		System.out.println("Maximum is " + max); // printing max of all three
 	}
 }
